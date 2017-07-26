41748e93d27d5ab6801e6ff286cc245ca36a466a
Kshitij Lauria <kslauria@gmail.com>
2017-07-26 00:15:01 -0400

    .

--- pwm-attiny85.c
+++ pwm-attiny85.c
@@ -8,7 +8,10 @@ int main (void)
 	DDRB = (1 << DDB0);
 
   // Table 11-5. Select Phase correct PWM with no prescaler and top =
-  // 0xFF. What is the other TOP setting for?
+  // 0xFF. What is the other TOP setting for? Apparently in Mode 5,
+  // OCR0A is the TOP value and you can only get a PWM on OC0B. Why
+  // would you want that? Maybe to control the frequency as well as
+  // duty cycle.
   TCCR0A = (1 << COM0A1) | (1 << WGM00);
   TCCR0B = (1 << CS00);
   OCR0A  = 50;
